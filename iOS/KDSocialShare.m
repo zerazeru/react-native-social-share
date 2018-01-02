60751007c53270e55d3b08ddd476d030e61c4cb8
zerazeru <zerazeru@users.noreply.github.com>
2018-01-02 11:23:21 +0100

    Remove isAvailableForServiceType check (always false on ios 11)

--- iOS/KDSocialShare.m
+++ iOS/KDSocialShare.m
@@ -24,11 +24,6 @@ RCT_EXPORT_MODULE()
      options:(NSDictionary *)options
     callback:(RCTResponseSenderBlock)callback
 {
-  if (![SLComposeViewController isAvailableForServiceType:serviceType]) {
-    callback(@[@"not_available"]);
-    return;
-  }
-
   SLComposeViewController *composeCtl = [SLComposeViewController composeViewControllerForServiceType:serviceType];
 
   if ([options objectForKey:@"link"] && [options objectForKey:@"link"] != [NSNull null]) {
